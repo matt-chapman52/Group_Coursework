db42ac35894ce96e14ff16175b273daf168ebe39
Ollie Taylor <oliver.a.taylor@dyson.com>
2020-03-27 18:12:41 +0000

    V2.1 - Polyfit and added height of table variable

--- Part_2.m
+++ Part_2.m
@@ -3,16 +3,17 @@ clear
 
 dataFile = "HospitalData3.xlsx";                        % Stores data set filename
 dataSet = readtable(dataFile);                          % Reads the dataset and stores data into a table
+height = height(dataSet);
 
 percentageSet = struct;                                 % Initialises percentageSet as a struct
 populations = table2array(dataSet(:,2));                % Converts the table section containing population data into an array and stores to "populations2 array
 
-months = [1:96];                                        % Constructs array of intergers from 1 to 96 (length of dataset) **NOTE could use length()?
+months = [1:height];                                        % Constructs array of intergers from 1 to 96 (length of dataset) **NOTE could use length()?
 
-for i = 1:96;                                           % Main loop iterates through each data point of the set  
+for i = 1:height;                                           % Main loop iterates through each data point of the set  
 daysNums = dataSet{i, 3:9};                             % Extracts the data containing the patient numbers for each day of the week
 daysNumsAll(i) = sum(daysNums);                         % Sums the daysnums array to find total monthly patients 
-monthlyPercentage = (sum(daysNums)/ populations(i))*100;   % Calculates and stores the average monthly patients to the percentage struct **NOTE use daysnumsAll
+monthlyPercentage = (daysNumsAll(i)/ populations(i))*100;   % Calculates and stores the average monthly patients to the percentage struct 
 percentageSet(i).monthPercent = monthlyPercentage;
 
     for j = 1:7;                                        % Sub loop calculates percentage of population by day and stores to percentage set struct
@@ -21,10 +22,21 @@ percentageSet(i).monthPercent = monthlyPercentage;
    
 end
 
-plot(months, [percentageSet(1:96).monthPercent]);       % Plots month-by-month percentage against months
+plot(months, [percentageSet(1:height).monthPercent]);       % Plots month-by-month percentage against months
 figure;
 plot(months, populations);                              % Plots population aginst months
 xlabel("Months After First Data Point");
 ylabel("Population of Town")
 yyaxis right;
 plot(months, daysNumsAll);                              % Plots patients in hospital against months
+
+hold all;
+
+p = polyfit(months, daysNumsAll, 1);
+y1 = polyval(p, months);
+plot(months, y1);
+ylabel("Patients In Hospital By Month");
+
+for k = 1:8;
+    xline(12*k);
+end
